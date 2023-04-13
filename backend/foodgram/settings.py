13327cc9b11fbefa2c0ca5c5ca442e2aa593336b
Андреев Петр <andreev.pete@gmail.com>
2023-04-13 18:31:03 +0300

    a

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -74,7 +74,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 REST_FRAMEWORK = {
