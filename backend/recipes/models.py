4bd8c92b19d6fe01edc73a0494da50ac8e96ccc3
Андреев Петр <andreev.pete@gmail.com>
2023-04-14 01:27:16 +0300

    a

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -10,7 +10,7 @@ class Ingredient(models.Model):
         max_length=200,
         verbose_name='Ингридиент'
     )
-    measurument_unit = models.CharField(
+    measurement_unit = models.CharField(
         max_length=200,
         verbose_name='Единица измерения'
     )
